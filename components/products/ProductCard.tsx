28e4411347b82e3bf97f805b9a6c6c98bbcf99f5
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-20 19:03:02 -0300

    mostrar segunda img cuando estamos encima + fadeIn Cards

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,21 +1,46 @@
-import { FC } from "react";
-import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material';
+import { FC, useMemo, useState } from "react";
+import {
+   Grid,
+   Card,
+   CardActionArea,
+   CardMedia,
+   Box,
+   Typography,
+} from "@mui/material";
 import { IProduct } from "../../interfaces";
+import 'animate.css';
 
 interface Props {
    product: IProduct;
 }
 
 export const ProductCard: FC<Props> = ({ product }) => {
+   const [isHovered, setIsHovered] = useState(false);
+   const checked = isHovered;
+   // Memorizamos el productImage porque puede cambiar otro estado y no quiero que se vuelva a recalcular cada vez.
+   const productImage = useMemo(() => {
+      return isHovered
+         ? `products/${product.images[1]}`
+         : `products/${product.images[0]}`;
+   }, [isHovered, product.images]);
+
    return (
-      <Grid item xs={6} sm={4}>
+      <Grid
+         item
+         xs={6}
+         sm={4}
+         onMouseEnter={() => setIsHovered(true)}
+         onMouseLeave={() => setIsHovered(false)}
+      >
          <Card>
             <CardActionArea>
-               <CardMedia
-                  component="img"
-                  image={`products/${product.images[0]}`}
-                  alt={product.title}
-               />
+                  <CardMedia
+                     className="animate__animated animate__fadeIn"
+                     component="img"
+                     image={productImage}
+                     alt={product.title}
+                     // className="fadeIn"
+                  />
             </CardActionArea>
          </Card>
          <Box sx={{ mt: 1 }} className="fadeIn">
