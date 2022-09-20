21e1d261d17410bd3ecaba015046610affab7c62
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-20 10:08:31 -0300

    ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from "next";
+import { Typography } from "@mui/material";
+
+import { ShopLayout } from "../components/layouts";
 
 const Home: NextPage = () => {
-   return <h1>Hola mundo</h1>;
+   return (
+      <ShopLayout
+         title={"Teslo-Shop - Home"}
+         pageDescription={"Encuentra los mejores productos de Teslo"}
+      >
+         <Typography variant="h1" component="h1">
+            Tienda
+         </Typography>
+         <Typography variant="h2" sx={{ mb: 1 }}>
+            Todos los productos
+         </Typography>
+      </ShopLayout>
+   );
 };
 
 export default Home;
