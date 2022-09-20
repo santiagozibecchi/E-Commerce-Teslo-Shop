8bc299aedc2e776b5412cd2f86feadbf5e9a23d5
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-20 11:20:55 -0300

    Home Page - Temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
-import { Typography } from "@mui/material";
+import {
+   Typography,
+   Grid,
+   Card,
+   CardActionArea,
+   CardMedia,
+} from "@mui/material";
 
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
    return (
@@ -15,6 +22,22 @@ const Home: NextPage = () => {
          <Typography variant="h2" sx={{ mb: 1 }}>
             Todos los productos
          </Typography>
+
+         <Grid container spacing={4}>
+            {initialData.products.map((product) => (
+               <Grid item xs={6} sm={4} key={product.slug}>
+                  <Card>
+                     <CardActionArea>
+                        <CardMedia
+                           component="img"
+                           image={`products/${product.images[0]}`}
+                           alt={product.title}
+                        />
+                     </CardActionArea>
+                  </Card>
+               </Grid>
+            ))}
+         </Grid>
       </ShopLayout>
    );
 };
