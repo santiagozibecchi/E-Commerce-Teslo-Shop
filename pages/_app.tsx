843f6df16b3eccf21a7d5c4d802322c1ea46d186
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-20 08:52:00 -0300

    styles-database-public-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+   return;
+   <ThemeProvider theme={}>
+      <Component {...pageProps} />
+   </ThemeProvider>;
 }
 
-export default MyApp
+export default MyApp;
