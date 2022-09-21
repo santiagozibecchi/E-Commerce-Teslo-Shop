8296241df97121226a1d0e44e4ab59fe8665fdfb
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-21 18:26:18 -0300

    Mostrar productos del carrito de compras

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,6 +1,16 @@
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import NextLink from "next/link";
+import {
+   Box,
+   Button,
+   CardActionArea,
+   CardMedia,
+   Grid,
+   Link,
+   Typography,
+} from "@mui/material";
 import { initialData } from "../../database/products";
+import { ItemCounter } from "../ui";
 // Este componente debe recibir de algun o leerlo de lugar, los productos que voy a mostrar en el carrito de compras.
 
 const productsInCart = [
@@ -13,7 +23,45 @@ export const CartList: FC = () => {
    return (
       <>
          {productsInCart.map((product) => (
-            <Typography key={product.slug}>{product.title}</Typography>
+            <Grid container spacing={2} sx={{ mb: 1 }} key={product.slug}>
+               <Grid item xs={3}>
+                  {/* TODO: Llevar a la pagina del producto */}
+                  <NextLink href="/product/slug" passHref>
+                     <Link>
+                        <CardActionArea>
+                           <CardMedia
+                              image={`products/${product.images[0]}`}
+                              component="img"
+                              sx={{ borderRadius: "5px" }}
+                           />
+                        </CardActionArea>
+                     </Link>
+                  </NextLink>
+               </Grid>
+               <Grid item xs={6}>
+                  <Box display="flex" flexDirection="column">
+                     <Typography variant="body2">{product.title}</Typography>
+                     <Typography variant="body2">
+                        Talla: <strong>M</strong>
+                     </Typography>
+
+                     {/* Condicional ya que en un determinado punto no podra volver a modificar la cantidad*/}
+                     <ItemCounter />
+                  </Box>
+               </Grid>
+               <Grid
+                  item
+                  xs={3}
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center"
+               >
+                  <Typography variant="subtitle1">${product.price}</Typography>
+                  <Button variant="text" color="secondary">
+                     Remover
+                  </Button>
+               </Grid>
+            </Grid>
          ))}
       </>
    );
