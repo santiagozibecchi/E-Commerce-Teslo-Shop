6f610aac24f8c862269decb1bdcaf71948f85785
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-22 14:11:09 -0300

    auth/login Inicio de sesion

---
+++ components/layouts/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+import { Box } from "@mui/material";
+import styles from "./AuthLayout.module.css";
+
+interface Props {
+   title: string;
+}
+
+export const AuthLayout: FC<PropsWithChildren<Props>> = ({
+   children,
+   title,
+}) => {
+   return (
+      <>
+         <Head>
+            <title>{title}</title>
+         </Head>
+
+         <main className={styles.authLayout}>
+            <Box
+               display="flex"
+               justifyContent="center"
+               alignItems="center"
+               height="calc(100vh - 200px)"
+            >
+               {children}
+            </Box>
+         </main>
+      </>
+   );
+};
---
+++ pages/auth/login.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import NextLink from "next/link";
+import { AuthLayout } from "../../components/layouts";
+import {
+   Box,
+   Button,
+   Grid,
+   Link,
+   TextField,
+   Typography,
+   styled,
+} from "@mui/material";
+import styles from "./Login.module.css";
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+   "& fieldset": {
+      borderRadius: "20px",
+      paddingLeft: theme.spacing(2.5),
+   },
+   "&:hover fieldset": {
+      borderColor: theme.palette.secondary.main,
+   },
+   "& label.Mui-focused": {
+      color: theme.palette.secondary.main,
+   },
+   "& .MuiInput-underline:after": {
+      borderBottomColor: theme.palette.secondary.main,
+   },
+   "& .MuiOutlinedInput-root": {
+      // "& fieldset": {
+      //    borderColor: theme.palette.secondary.main,
+      // },
+      "&.Mui-focused fieldset": {
+         borderColor: theme.palette.secondary.main,
+      },
+   },
+   "& label": {
+      paddingLeft: theme.spacing(2),
+   },
+   "& input": {
+      paddingLeft: theme.spacing(3.5),
+   },
+}));
+
+const LoginPage = () => {
+   return (
+      <AuthLayout title={"Ingresar"}>
+         <Box className={styles.loginForm}>
+            <Box
+               sx={{
+                  width: 350,
+                  padding: "15px 20px",
+               }}
+            >
+               <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                     <Typography
+                        variant="h4"
+                        component="h1"
+                        color="secondary"
+                        fontWeight="bold"
+                     >
+                        Iniciar Sesión
+                     </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                     <StyledTextField
+                        label="Correo"
+                        variant="outlined"
+                        fullWidth
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <StyledTextField
+                        label="Contraseña"
+                        variant="outlined"
+                        fullWidth
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <Button
+                        color="secondary"
+                        className="circular-btn"
+                        size="large"
+                        fullWidth
+                     >
+                        Ingresar
+                     </Button>
+                  </Grid>
+                  <Grid item xs={12} display="flex" justifyContent="flex-end">
+                     <NextLink href="/auth/register" passHref>
+                        <Link underline="always">¿No tienes cuenta?</Link>
+                     </NextLink>
+                  </Grid>
+               </Grid>
+            </Box>
+         </Box>
+      </AuthLayout>
+   );
+};
+
+export default LoginPage;
