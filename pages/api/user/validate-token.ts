6ec45bba682cc5c853ae140c882f6914bb39cb35
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-08 11:56:10 -0300

    Validar y renovar JWT

---
+++ pages/api/user/validate-token.ts
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { User } from "../../../models";
+import bcrypt from "bcryptjs";
+import { jwt } from "../../../utils";
+
+type Data =
+   | {
+        message: string;
+     }
+   | {
+        token: string;
+        user: { email: string; role: string; name: string };
+     };
+
+export default function handler(
+   req: NextApiRequest,
+   res: NextApiResponse<Data>
+) {
+   switch (req.method) {
+      case "GET":
+         return checkjwt(req, res);
+
+      default:
+         res.status(400).json({
+            message: "Bad Request",
+         });
+   }
+}
+
+const checkjwt = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+   const { token = "" } = req.cookies;
+
+   let userId: string = "";
+
+   try {
+      userId = await jwt.isValidToken(token);
+   } catch (error) {
+      return res.status(401).json({
+         message: "Token de autorización no es válido",
+      });
+   }
+
+   await db.connect();
+
+   const user = await User.findById(userId).lean();
+   await db.disconnect();
+
+   if (!user) {
+      return res.status(400).json({
+         message: "No existe usuario con ese ID",
+      });
+   }
+
+   const { email, _id, role, name } = user;
+
+   return res.status(200).json({
+      token: jwt.signToken(_id, email),
+      user: {
+         email,
+         role,
+         name,
+      },
+   });
+};
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const signToken = (_id: string, email: string) => {
+   // Validacion de seguridad
    if (!process.env.JWT_SECRET_SEED) {
       throw new Error("No hay semilla de JWT - Revisar variables de entorno");
    }
@@ -18,4 +19,27 @@ export const signToken = (_id: string, email: string) => {
          expiresIn: "30d",
       }
    );
+};
+
+export const isValidToken = (token: string): Promise<string> => {
+   if (!process.env.JWT_SECRET_SEED) {
+      throw new Error("No hay semilla de JWT - Revisar variables de entorno");
+   }
+
+   return new Promise((resolve, reject) => {
+      try {
+         jwt.verify(
+            token,
+            process.env.JWT_SECRET_SEED || "",
+            (err, payload) => {
+               if (err) return reject("JWT no es válido");
+
+               const { _id } = payload as { _id: string };
+               resolve(_id);
+            }
+         );
+      } catch (error) {
+         return reject("JWT no es válido");
+      }
+   });
 };
