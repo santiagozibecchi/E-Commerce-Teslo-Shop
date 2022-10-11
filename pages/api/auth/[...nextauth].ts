8cb5b9f091f74a6624e9a93766b176bf49681dde
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-11 15:48:59 -0300

    NextAuth Callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
    // Configure one or more authentication providers
    providers: [
-      GithubProvider({
-         clientId: process.env.GITHUB_ID as string,
-         clientSecret: process.env.GITHUB_SECRET as string,
-      }),
       // ...add more providers here
       Credentials({
          name: "Custom Login",
@@ -22,15 +18,51 @@ export const authOptions = {
          },
          // regresa null en caso de que la autenticacion falle o un objeto si todo pasa. Este tiene que tener la informacion que nosotros queremos que sea recibida para crear los token de autenticacion.
          async authorize(credentials) {
-            console.log(credentials);
+            console.log({ credentials });
             // TODO Validar contra base de datos
 
-            return null;
+            return { name: "juan", correo: "juan@gmail.com", role: "admin" };
          },
+      }),
+      GithubProvider({
+         clientId: process.env.GITHUB_ID as string,
+         clientSecret: process.env.GITHUB_SECRET as string,
       }),
    ],
 
    // Callbacks
-   callbacks: {},
+   // Como quiero que se firme el jwt, que data es la qie voy a grabar en los token
+   callbacks: {
+      async jwt({ token, account, user }: any) {
+         // console.log("------------------ JWT -----------------");
+         // console.log({ token, account, user });
+         if (account) {
+            token.accessToken = account.access_token;
+
+            switch (account.type) {
+               case "credentials":
+                  token.user = user;
+                  break;
+
+               case "oauth":
+                  // TODO crear usuario o verificar si existe en DB
+                  break;
+
+               default:
+                  break;
+            }
+         }
+         return token;
+      },
+      // Cuando se genera una nueva session
+      async session({ session, token, user }: any) {
+         // console.log("------------- SESSION --------------------");
+         // console.log({ session, token, user });
+         session.accessToken = token.accessToken;
+         session.user = token.user as any;
+
+         return session;
+      },
+   },
 };
 export default NextAuth(authOptions);
