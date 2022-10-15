1904b97ea376434a84de3f70970702084cbe6aa3
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-14 21:25:54 -0300

    Crear orden - Backend Parte 2

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { db } from "../../../database";
 import { IOrder } from "../../../interfaces";
-import { Product } from "../../../models";
+import { Product, Order } from "../../../models";
 
-type Data = {
-   message: string;
-};
+type Data =
+   | {
+        message: string;
+     }
+   | IOrder;
 
 export default function handler(
    req: NextApiRequest,
@@ -25,7 +27,7 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
    const { orderItems, total } = req.body as IOrder;
 
    // Verificar que tengamos un usuario
-   const session = await getSession({ req });
+   const session: any = await getSession({ req });
 
    if (!session) {
       return res.status(401).json({
@@ -42,8 +44,9 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
    try {
       const subTotal = orderItems.reduce((prev, current) => {
          // Verificando que tanto el id de la db como el que viene del front coincidan para acceder al precio del producto del id
+         // ! Tener en cuenta que estamos trabajando en el ObjectId de mongo => p.id para acceder al id string
          const currentPrice = dbProducts.find(
-            (p) => p._id === current._id
+            (p) => p.id === current._id
          )!.price;
          if (!currentPrice) {
             throw new Error(
@@ -53,9 +56,30 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
          return currentPrice * current.quantity + prev;
       }, 0);
-   } catch (error) {}
 
-   await db.disconnect();
+      const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
 
-   res.status(201).json(req.body);
+      const backendTotal = subTotal * (taxRate + 1);
+
+      if (total !== backendTotal) {
+         throw new Error("El total no cuadra con el monto");
+      }
+
+      // * En este punto ya sería una orden permitida:
+
+      const userId = session.user._id;
+      const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+
+      await newOrder.save();
+
+      return res.status(201).json(newOrder);
+   } catch (error) {
+      await db.disconnect();
+      console.log(error);
+      res.status(400).json({
+         message: "Revise logs del servidor",
+      });
+   }
+
+   //    res.status(201).json(req.body);
 };
