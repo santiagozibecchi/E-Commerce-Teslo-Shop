fab3901e43b91bdad67d759a52d6b6a31f9a2c59
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-19 10:10:40 -0300

    Middlewares para validar autenticación - API and admin pages

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 // import { withAuth } from "next-auth/middleware";
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 // import { NextResponse, NextRequest } from "next/server";
 // import { getToken } from "next-auth/jwt";
 
@@ -31,6 +31,31 @@ export { default } from "next-auth/middleware";
 //    return NextResponse.next();
 // });
 
+// export const config = {
+//    matcher: "/checkout/:path*",
+// };
+
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+   // `withAuth` augments your `Request` with the user's token.
+   function middleware(req) {
+      // console.log(req.nextauth);
+      const { role } = req.nextauth.token?.user as any;
+      const validRoles = ["admin", "super-user", "seo"];
+
+      if (!validRoles.includes(role)) {
+         return NextResponse.redirect(new URL("/", req.url));
+      }
+   },
+   {
+      callbacks: {
+         authorized: ({ token }) => !!token,
+      },
+   }
+);
+
 export const config = {
-   matcher: "/checkout/:path*",
+   matcher: ["/checkout/:path*", "/admin/:path*", "/api/admin/:path*"],
 };
