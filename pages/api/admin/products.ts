8ed7b7505c94bdc10eb13559d08ea452c489dec8
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-26 08:44:20 -0300

    REST - GetProductos

---
+++ pages/api/admin/products.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { IProduct } from "../../../interfaces";
+import { Product } from "../../../models";
+
+type Data =
+   | {
+        message: string;
+     }
+   | IProduct[];
+
+export default function handler(
+   req: NextApiRequest,
+   res: NextApiResponse<Data>
+) {
+   switch (req.method) {
+      case "GET":
+         return getProducts(req, res);
+      case "PUT":
+      case "POST":
+
+      default:
+         res.status(400).json({ message: "Bad Request" });
+   }
+}
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+   await db.connect();
+
+   const products = await Product.find().sort({ title: "asc" }).lean();
+
+   await db.disconnect();
+   // TODO
+   // Tendremos que actualizar las imagenes => cloudynary
+
+   return res.status(200).json(products);
+};
