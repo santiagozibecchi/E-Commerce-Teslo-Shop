e0afa8dd3d81a8d4aced41ff5bb92438823a2ee4
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-10-27 20:19:04 -0300

    REST - Actualizar Producto

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,12 +2,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../database";
 import { IProduct } from "../../../interfaces";
 import { Product } from "../../../models";
+import { isValidObjectId } from "mongoose";
 
 type Data =
    | {
         message: string;
      }
-   | IProduct[];
+   | IProduct[]
+   | IProduct;
 
 export default function handler(
    req: NextApiRequest,
@@ -17,6 +19,7 @@ export default function handler(
       case "GET":
          return getProducts(req, res);
       case "PUT":
+         return updateProduct(req, res);
       case "POST":
 
       default:
@@ -33,4 +36,54 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
    // Tendremos que actualizar las imagenes => cloudynary
 
    return res.status(200).json(products);
+};
+
+const updateProduct = async (
+   req: NextApiRequest,
+   res: NextApiResponse<Data>
+) => {
+   // tengo que asegurarme de que siempre vengan al menos 2 imagenes
+
+   const { _id = "", images = [] } = req.body as IProduct;
+
+   if (isValidObjectId(_id)) {
+      return res.status(400).json({
+         message: "El id del producto no es válido",
+      });
+   }
+
+   if (images.length <= 2) {
+      return res.status(400).json({
+         message: "Es necesario al menos 2 imágenes",
+      });
+   }
+
+   // TODO: tendremos localhost:3000/products/imasd43afa.jpg
+   // * Los paths relativos no sirven a la hora de hacer socialShearing
+
+   try {
+      await db.connect();
+
+      const product = await Product.findById(_id);
+
+      if (!product) {
+         await db.disconnect();
+         return res.status(400).json({
+            message: "No existe un producto con ese id",
+         });
+      }
+
+      // TODO: eliminar fotos en Cloudinary
+
+      await product.update(req.body);
+      await db.disconnect();
+
+      return res.status(200).json(product);
+   } catch (error) {
+      console.log(error);
+      await db.disconnect();
+      return res.status(400).json({
+         message: "Revisar la consola del servidor",
+      });
+   }
 };
